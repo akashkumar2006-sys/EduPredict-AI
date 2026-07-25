e053b52bf1947d13ea6d6f2edce161419676842f
Akash Kumar Jha <akashkumarrolex2006@gmail.com>
2026-07-25 13:31:31 +0530

    Update predict function for input handling
    Refactor predict function to handle both JSON and form data inputs.

--- app.py
+++ app.py
@@ -1,27 +1,3 @@
-from flask import Flask, render_template, request, jsonify
-import joblib
-import pandas as pd
-import os
-
-app = Flask(__name__)
-
-
-# Load model
-model = None
-
-model_path = "model/model.pkl"
-
-if os.path.exists(model_path):
-    model = joblib.load(model_path)
-
-
-
-@app.route("/")
-def home():
-    return render_template("index.html")
-
-
-
 @app.route("/predict", methods=["POST"])
 def predict():
 
@@ -31,37 +7,39 @@ def predict():
         })
 
 
-    data = request.get_json()
+    if request.is_json:
+        data = request.get_json()
+
+        hours = data["hours_studied"]
+        previous = data["previous_score"]
+        attendance = data["attendance"]
+        sleep = data["sleep_hours"]
+        papers = data["sample_papers"]
+
+    else:
+        hours = request.form["hours"]
+        previous = request.form["previous"]
+        attendance = request.form["attendance"]
+        sleep = request.form["sleep"]
+        papers = request.form["papers"]
 
 
     input_data = {
 
-        "Hours_Studied": float(data["hours_studied"]),
-
-        "Previous_Score": float(data["previous_score"]),
-
-        "Attendance": float(data["attendance"]),
-
-        "Sleep_Hours": float(data["sleep_hours"]),
-
-        "Sample_Papers_Practiced": float(data["sample_papers"])
+        "Hours_Studied": float(hours),
+        "Previous_Score": float(previous),
+        "Attendance": float(attendance),
+        "Sleep_Hours": float(sleep),
+        "Sample_Papers_Practiced": float(papers)
 
     }
 
 
     df = pd.DataFrame([input_data])
 
-
     prediction = model.predict(df)[0]
 
 
     return jsonify({
-
         "prediction": round(float(prediction),2)
-
     })
-
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
